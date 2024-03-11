5404bd989969989a66356702453b42869067736c
WhyNotKo <112894857+WhyNotKo@users.noreply.github.com>
2024-03-11 22:29:49 +0300

    pre-last version

--- Aleshko_lab1/Aleshko_lab1/Aleshko_console/Aleshko_console.cpp
+++ Aleshko_lab1/Aleshko_lab1/Aleshko_console/Aleshko_console.cpp
@@ -1,20 +1,126 @@
-﻿// Aleshko_console.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#define _AFXDLL
 
-#include <iostream>
+#include "pch.h"
+#include "Session.h"
+
+#include <afxmt.h>
+
+using namespace std;
+
+inline void DoWrite()
+{
+	std::cout << std::endl;
+}
+
+template <class T, typename... Args> inline void DoWrite(T& value, Args... args)
+{
+	std::cout << value << " ";
+	DoWrite(args...);
+}
+
+static CCriticalSection cs;
+template <typename... Args> inline void SafeWrite(Args... args)
+{
+	cs.Lock();
+	DoWrite(args...);
+	cs.Unlock();
+}
+
+DWORD WINAPI workFunc(LPVOID _param)
+{
+	auto session = static_cast<Session*>(_param);
+	SafeWrite("session", session->sessionID, "created");
+	while (true)
+	{
+		Message m;
+		if (session->getMessage(m))
+		{
+			switch (m.header.messageType)
+			{
+			case MT_CLOSE:
+			{
+				SafeWrite("session", session->sessionID, "closed");
+				delete session;
+				return 0;
+			}
+			case MT_DATA:
+			{
+				SafeWrite("session", session->sessionID, "data", m.data);
+				Sleep(100 * session->sessionID);
+				break;
+			}
+			}
+		}
+	}
+	return 0;
+}
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	HANDLE confirmEvent = ::CreateEvent(NULL, FALSE, FALSE, "ConfirmEvent");
+	HANDLE startEvent = ::CreateEvent(NULL, FALSE, FALSE, "StartEvent");
+	HANDLE stopEvent = ::CreateEvent(NULL, FALSE, FALSE, "CloseProc");
+	HANDLE exitEvent = ::CreateEvent(NULL, FALSE, FALSE, "ExitProc");
+
+
+	HANDLE events[3];
+	events[0] = exitEvent;
+	events[1] = stopEvent;
+	events[2] = startEvent;
+
+	::SetEvent(confirmEvent);
+
+	std::vector<HANDLE> hThreads;
+	std::vector<Session*> sessions;
+
+	while (true)
+	{
+		DWORD dwWaitRes = ::WaitForMultipleObjects(sizeof(events) / sizeof(HANDLE), events, FALSE, INFINITE);
+		switch (dwWaitRes)
+		{
+		case WAIT_OBJECT_0:
+			for (int i{ 0 }; i < sessions.size(); ++i)
+			{
+				sessions[i]->addMessage(MT_CLOSE);
+				CloseHandle(hThreads[i]);
+			}
+			return 0;
+		case WAIT_OBJECT_0 + 1:
+
+			if (!hThreads.size())
+			{
+				::SetEvent(confirmEvent);
+				return 0;
+			}
+			sessions.back()->addMessage(MT_CLOSE);
+			sessions.pop_back();
+
+			::CloseHandle(hThreads.back());
+			hThreads.pop_back();
+
+			break;
+
+		case WAIT_OBJECT_0 + 2:
+
+			sessions.push_back(new Session(hThreads.size()));
+			hThreads.push_back(::CreateThread(NULL, 0, workFunc, (LPVOID)sessions.back(), 0, NULL));
+			break;
+
+		default:
+
+			std::cout << "There was an error" << std::endl;
+			return 0;
+		}
+		::SetEvent(confirmEvent);
+	}
+
+	WaitForMultipleObjects((DWORD)hThreads.size(), hThreads.data(), TRUE, INFINITE);
+
+	CloseHandle(confirmEvent);
+	CloseHandle(startEvent);
+	CloseHandle(stopEvent);
+
+	return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
--- Aleshko_lab1/Aleshko_lab1/Aleshko_lab1/Form1.cs
+++ Aleshko_lab1/Aleshko_lab1/Aleshko_lab1/Form1.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,11 +14,17 @@ namespace Aleshko_lab1
 {
     public partial class Form1 : Form
     {
+        Process childProcess = null;
+        EventWaitHandle eventStart = new EventWaitHandle(false, EventResetMode.AutoReset, "StartEvent");
+        EventWaitHandle eventConfirm = new EventWaitHandle(false, EventResetMode.AutoReset, "ConfirmEvent");
+        EventWaitHandle eventClose = new EventWaitHandle(false, EventResetMode.AutoReset, "CloseProc");
+        EventWaitHandle eventExit = new EventWaitHandle(false, EventResetMode.AutoReset, "ExitProc");
         public Form1()
         {
             InitializeComponent();
         }
 
+        int threadsCounter = 0;
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -29,12 +37,68 @@ namespace Aleshko_lab1
 
         private void btn_start_Click(object sender, EventArgs e)
         {
+            if (childProcess == null || childProcess.HasExited)
+            {
+                listBox1.Items.Clear();
+                childProcess = Process.Start("Aleshko_console.exe");
+                eventConfirm.WaitOne();
+                listBox1.Items.Add("Основной");
+                listBox1.Items.Add("Все потоки");
+                threadsCounter++;
 
+            }
+            else
+            {
+                int n = Convert.ToInt32(numericUpDown1.Value);
+
+                for (int i = 0; i < n; ++i)
+                {
+                    eventStart.Set();
+                    eventConfirm.WaitOne();
+                    listBox1.Items.Add(threadsCounter++).ToString();
+                }
+            }
         }
 
         private void btn_stop_Click(object sender, EventArgs e)
         {
-
+            if (childProcess == null || childProcess.HasExited)
+            {
+                listBox1.Items.Clear();
+            }
+            else
+            {
+                if (listBox1.Items.Count > 2)
+                {
+                    int n = Convert.ToInt32(numericUpDown1.Value);
+                    if(n < threadsCounter)
+                    {
+                        for (int i = 0; i < n; ++i)
+                        {
+                            eventClose.Set();
+                            eventConfirm.WaitOne();
+                            listBox1.Items.RemoveAt(threadsCounter);
+                            threadsCounter--;
+                        }
+                    }
+                    else
+                    {
+                        eventClose.Set();
+                        eventConfirm.WaitOne();
+                        listBox1.Items.RemoveAt(threadsCounter);
+                        threadsCounter--;
+                    }
+                    
+                }
+                else
+                {
+                    eventClose.Set();
+                    eventConfirm.WaitOne();
+                    listBox1.Items.Clear();
+                    threadsCounter = 0;
+                }
+                
+            }
         }
 
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
